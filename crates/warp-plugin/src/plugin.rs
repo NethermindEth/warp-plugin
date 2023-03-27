1520df7adea36724d8ea0de7435eed17401cba1f
rodrigo-pino <rodrodpino@gmail.com>
2023-03-27 16:15:27 -0400

    Format to plugin.rs

--- crates/warp-plugin/src/plugin.rs
+++ crates/warp-plugin/src/plugin.rs
@@ -1,21 +1,26 @@
 use std::collections::{HashMap, HashSet};
-use std::sync::{Arc};
+use std::sync::Arc;
 
-use cairo_lang_defs::plugin::{DynGeneratedFileAuxData, GeneratedFileAuxData, MacroPlugin, PluginDiagnostic, PluginGeneratedFile, PluginResult};
+use cairo_lang_defs::plugin::{
+    DynGeneratedFileAuxData, GeneratedFileAuxData, MacroPlugin, PluginDiagnostic,
+    PluginGeneratedFile, PluginResult,
+};
 use cairo_lang_diagnostics::DiagnosticEntry;
 use cairo_lang_semantic::db::SemanticGroup;
 use cairo_lang_semantic::patcher::{PatchBuilder, Patches, RewriteNode};
-use cairo_lang_semantic::plugin::{AsDynGeneratedFileAuxData, AsDynMacroPlugin, DynPluginAuxData, PluginAuxData, PluginMappedDiagnostic, SemanticPlugin};
+use cairo_lang_semantic::plugin::{
+    AsDynGeneratedFileAuxData, AsDynMacroPlugin, DynPluginAuxData, PluginAuxData,
+    PluginMappedDiagnostic, SemanticPlugin,
+};
 use cairo_lang_semantic::SemanticDiagnostic;
-use cairo_lang_syntax::node::db::SyntaxGroup;
-use regex::Regex;
-use cairo_lang_syntax::node::{ast, SyntaxNode, Terminal, TypedSyntaxNode};
 use cairo_lang_syntax::node::ast::{FunctionWithBody, MaybeModuleBody, ModuleBody};
+use cairo_lang_syntax::node::db::SyntaxGroup;
+use cairo_lang_syntax::node::{ast, SyntaxNode, Terminal, TypedSyntaxNode};
+use regex::Regex;
 
 use smol_str::SmolStr;
 
-
-use crate::implicits::{handle_implicits};
+use crate::implicits::handle_implicits;
 
 /// Warp related auxiliary data of the Warp plugin.
 #[derive(Debug, PartialEq, Eq)]
@@ -32,7 +37,11 @@ impl GeneratedFileAuxData for WarpAuxData {
         self
     }
     fn eq(&self, other: &dyn GeneratedFileAuxData) -> bool {
-        if let Some(other) = other.as_any().downcast_ref::<Self>() { self == other } else { false }
+        if let Some(other) = other.as_any().downcast_ref::<Self>() {
+            self == other
+        } else {
+            false
+        }
     }
 }
 
@@ -49,10 +58,14 @@ impl PluginAuxData for WarpAuxData {
         diag: &dyn std::any::Any,
     ) -> Option<PluginMappedDiagnostic> {
         let Some(diag) = diag.downcast_ref::<SemanticDiagnostic>() else { return None; };
-        let span = self
-            .patches
-            .translate(db.upcast(), diag.stable_location.diagnostic_location(db.upcast()).span)?;
-        Some(PluginMappedDiagnostic { span, message: diag.format(db) })
+        let span = self.patches.translate(
+            db.upcast(),
+            diag.stable_location.diagnostic_location(db.upcast()).span,
+        )?;
+        Some(PluginMappedDiagnostic {
+            span,
+            message: diag.format(db),
+        })
     }
 }
 
@@ -83,11 +96,11 @@ impl WarpPlugin {
     /// indicating whether the original module should be removed.
     ///
     fn handle_mod(&self, db: &dyn SyntaxGroup, module_ast: &ast::ItemModule) -> PluginResult {
-        let module_name = module_ast.name(
-            db).text(db);
+        let module_name = module_ast.name(db).text(db);
         if let MaybeModuleBody::Some(module_body) = module_ast.body(db) {
             let attributes = module_ast.attributes(db).as_syntax_node();
-            let (rewrite_nodes, diagnostics) = self.handle_module_nodes(db, module_body, module_name.clone(), attributes);
+            let (rewrite_nodes, diagnostics) =
+                self.handle_module_nodes(db, module_body, module_name.clone(), attributes);
             return if let Some(module_rewritten) = rewrite_nodes {
                 let mut builder = PatchBuilder::new(db);
                 builder.add_modified(module_rewritten);
@@ -95,10 +108,12 @@ impl WarpPlugin {
                     code: Some(PluginGeneratedFile {
                         name: module_name.clone(),
                         content: builder.code,
-                        aux_data: DynGeneratedFileAuxData::new(DynPluginAuxData::new(WarpAuxData {
-                            patches: builder.patches,
-                            elements: vec![module_name],
-                        })),
+                        aux_data: DynGeneratedFileAuxData::new(DynPluginAuxData::new(
+                            WarpAuxData {
+                                patches: builder.patches,
+                                elements: vec![module_name],
+                            },
+                        )),
                     }),
                     diagnostics,
                     remove_original_item: true,
@@ -114,7 +129,6 @@ impl WarpPlugin {
 
         PluginResult::default()
     }
-
 
     /// Handles the syntax nodes of a Cairo function and returns a PluginResult containing the generated code
     /// and any diagnostic messages.
@@ -154,7 +168,10 @@ impl WarpPlugin {
                 .iter()
                 .filter(|(implicit_name, _)| {
                     let function_indices = get_file_function_indices(&parent_text, implicit_name);
-                    return function_indices.get(func_name.as_str()).map(|i| *i == 0).unwrap_or(false);
+                    return function_indices
+                        .get(func_name.as_str())
+                        .map(|i| *i == 0)
+                        .unwrap_or(false);
                 })
                 .map(|(_, implicit_import)| implicit_import.clone())
                 .collect();
@@ -183,7 +200,6 @@ impl WarpPlugin {
         };
     }
 
-
     /// Recursively handles the syntax nodes of a Cairo module's items and returns a tuple containing a
     /// `RewriteNode` and any diagnostic messages. The `RewriteNode` represents the modified module,
     /// including any generated code, required implicit imports, and original items. If no modifications are
@@ -203,56 +219,58 @@ impl WarpPlugin {
     ///
     /// A tuple containing a `RewriteNode` representing the modified module, and any diagnostic messages.
     ///
-    fn handle_module_nodes(&self, db: &dyn SyntaxGroup, module_body: ModuleBody, name: SmolStr, attributes: SyntaxNode) -> (Option<RewriteNode>, Vec<PluginDiagnostic>) {
+    fn handle_module_nodes(
+        &self,
+        db: &dyn SyntaxGroup,
+        module_body: ModuleBody,
+        name: SmolStr,
+        attributes: SyntaxNode,
+    ) -> (Option<RewriteNode>, Vec<PluginDiagnostic>) {
         let mut kept_original_items = vec![];
         let mut modified_functions = vec![];
         let mut modified_modules = vec![];
         let mut diagnostics = vec![];
         let mut imports: HashSet<String> = HashSet::new();
 
-        module_body
-            .items(db)
-            .elements(db)
-            .iter()
-            .for_each(|el| {
-                if let ast::Item::FreeFunction(function_ast) = el {
-                    let (rewrite_nodes, implicit_imports, implicit_diagnostics) = handle_implicits(db, function_ast);
-                    if let Some(implicit_functions_rewrite) = rewrite_nodes {
-                        modified_functions.push(implicit_functions_rewrite);
-                        let mapped_values: Vec<_> = implicit_imports.values().cloned().collect();
-                        imports.extend(mapped_values);
-                    }
-                    diagnostics.extend(implicit_diagnostics);
-                } else if let ast::Item::Module(module_ast) = el {
-                    let name = module_ast.name(
-                        db).text(db);
-
-                    if let MaybeModuleBody::Some(body) = module_ast.body(db) {
-                        let attributes = module_ast.attributes(db).as_syntax_node();
-                        let (rewrite_nodes, module_diagnostics) = self.handle_module_nodes(db, body, name, attributes);
-                        if let Some(module_rewrites) = rewrite_nodes {
-                            modified_modules.push(module_rewrites);
-                        }
-                        diagnostics.extend(module_diagnostics);
-                    }
-                } else {
-                    kept_original_items.push(RewriteNode::Copied(el.as_syntax_node()));
+        module_body.items(db).elements(db).iter().for_each(|el| {
+            if let ast::Item::FreeFunction(function_ast) = el {
+                let (rewrite_nodes, implicit_imports, implicit_diagnostics) =
+                    handle_implicits(db, function_ast);
+                if let Some(implicit_functions_rewrite) = rewrite_nodes {
+                    modified_functions.push(implicit_functions_rewrite);
+                    let mapped_values: Vec<_> = implicit_imports.values().cloned().collect();
+                    imports.extend(mapped_values);
                 }
-            });
+                diagnostics.extend(implicit_diagnostics);
+            } else if let ast::Item::Module(module_ast) = el {
+                let name = module_ast.name(db).text(db);
+
+                if let MaybeModuleBody::Some(body) = module_ast.body(db) {
+                    let attributes = module_ast.attributes(db).as_syntax_node();
+                    let (rewrite_nodes, module_diagnostics) =
+                        self.handle_module_nodes(db, body, name, attributes);
+                    if let Some(module_rewrites) = rewrite_nodes {
+                        modified_modules.push(module_rewrites);
+                    }
+                    diagnostics.extend(module_diagnostics);
+                }
+            } else {
+                kept_original_items.push(RewriteNode::Copied(el.as_syntax_node()));
+            }
+        });
 
         if modified_functions.is_empty() && modified_modules.is_empty() {
             return (None, diagnostics);
         }
-
 
         let imports_joined_newlines = imports
             .iter()
             .map(|s| s.to_string())
             .collect::<Vec<String>>()
             .join("\n");
-        (Some
-             (RewriteNode::interpolate_patched(
-                 "
+        (
+            Some(RewriteNode::interpolate_patched(
+                "
                 $attributes$
                 mod $name$ {
                     $implicit_imports$
@@ -261,24 +279,39 @@ impl WarpPlugin {
                     $modified_modules$
                 }
                 ",
-                 HashMap::from([
-                     ("attributes".to_string(), RewriteNode::new_trimmed(attributes)),
-                     ("implicit_imports".to_string(), RewriteNode::Text(imports_joined_newlines)),
-                     ("name".to_string(), RewriteNode::Text(name.to_string())),
-                     ("original_items".to_string(), RewriteNode::new_modified(kept_original_items)),
-                     ("modified_functions".to_string(), RewriteNode::new_modified(modified_functions)),
-                     ("modified_modules".to_string(), RewriteNode::new_modified(modified_modules)),
-                 ]),
-             )), diagnostics)
+                HashMap::from([
+                    (
+                        "attributes".to_string(),
+                        RewriteNode::new_trimmed(attributes),
+                    ),
+                    (
+                        "implicit_imports".to_string(),
+                        RewriteNode::Text(imports_joined_newlines),
+                    ),
+                    ("name".to_string(), RewriteNode::Text(name.to_string())),
+                    (
+                        "original_items".to_string(),
+                        RewriteNode::new_modified(kept_original_items),
+                    ),
+                    (
+                        "modified_functions".to_string(),
+                        RewriteNode::new_modified(modified_functions),
+                    ),
+                    (
+                        "modified_modules".to_string(),
+                        RewriteNode::new_modified(modified_modules),
+                    ),
+                ]),
+            )),
+            diagnostics,
+        )
     }
 }
 
 impl MacroPlugin for WarpPlugin {
     fn generate_code(&self, db: &dyn SyntaxGroup, item_ast: ast::Item) -> PluginResult {
         match &item_ast {
-            ast::Item::FreeFunction(func_ast) => {
-                self.handle_functions(db, func_ast)
-            }
+            ast::Item::FreeFunction(func_ast) => self.handle_functions(db, func_ast),
             ast::Item::Module(module_ast) => self.handle_mod(db, module_ast),
             _ => PluginResult::default(),
         }
@@ -287,8 +320,8 @@ impl MacroPlugin for WarpPlugin {
 
 impl AsDynMacroPlugin for WarpPlugin {
     fn as_dyn_macro_plugin<'a>(self: Arc<Self>) -> Arc<dyn MacroPlugin + 'a>
-        where
-            Self: 'a,
+    where
+        Self: 'a,
     {
         self
     }
@@ -310,7 +343,11 @@ impl SemanticPlugin for WarpPlugin {}
 /// the given implicit function name.
 ///
 fn get_file_function_indices(code: &str, implicit_name: &str) -> HashMap<String, usize> {
-    let re = Regex::new(&format!(r"#\[implicit\([^\)]*{}[^\)]*\)\]\s*fn\s+([a-zA-Z_][a-zA-Z0-9_]*)", implicit_name)).unwrap();
+    let re = Regex::new(&format!(
+        r"#\[implicit\([^\)]*{}[^\)]*\)\]\s*fn\s+([a-zA-Z_][a-zA-Z0-9_]*)",
+        implicit_name
+    ))
+    .unwrap();
     let mut function_indices = HashMap::new();
     let mut index = 0;
 
